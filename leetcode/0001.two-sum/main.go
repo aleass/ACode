6c204d25fcc077631af4f1cdd5f8eca5c0997ddf
Tuski <38355930+aleass@users.noreply.github.com>
2022-07-19 00:41:19 +0800

    feature(0001): two-sum
    Signed-off-by: tuski <g614547908@gmail.com>

---
+++ leetcode/0001.two-sum/main.go
@@ -0,0 +1,45 @@
+package main
+
+import "fmt"
+
+func main() {
+	var params = []int{2, 7, 11, 15}
+	var taget = 9
+	res := twoSum(params, taget)
+	fmt.Println(params[res[0]], params[res[1]], taget)
+
+	params = []int{3, 2, 4}
+	taget = 6
+	res = twoSum2(params, taget)
+	fmt.Println(params[res[0]], params[res[1]], taget)
+
+	params = []int{3, 2, 4}
+	taget = 6
+	res = twoSum2(params, taget)
+	fmt.Println(params[res[0]], params[res[1]], taget, res)
+}
+
+func twoSum2(nums []int, target int) []int {
+	for i, num := range nums {
+		for j := i + 1; j < len(nums); j++ {
+			if num+nums[j] == target {
+				return []int{i, j}
+			}
+		}
+	}
+	return nil
+}
+
+func twoSum(nums []int, target int) []int {
+	m := make(map[int]int, 100)
+	m[nums[0]] = 0
+	for i := 1; i < len(nums); i++ {
+		num := nums[i]
+		if i2, ok := m[target-num]; ok {
+			return []int{i, i2}
+		} else {
+			m[num] = i
+		}
+	}
+	return nil
+}
