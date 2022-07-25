1b6106a322371d19adc00dd65ecc43d40f1a2774
guanziliang <gzljszgl@qq.com>
2022-07-25 14:13:55 +0800

    refactor(0007): edit

--- leetcode/Go/0007.reverse-integer/main.go
+++ leetcode/Go/0007.reverse-integer/main.go
@@ -2,29 +2,42 @@ package main
 
 import (
 	"fmt"
+	"math"
 	"strconv"
 )
 
 func main() {
-	fmt.Println(reverse(1234))
+	fmt.Println(reverse(123456))
 }
 func reverse(x int) int {
-	s := strconv.Itoa(x)
-	isO := false
-	if s[0] == '-' {
-		s = s[1:]
-		isO = true
+	var minus bool
+	if x < 0 {
+		minus = true
+		x = -x
 	}
-	s1 := make([]byte, len(s))
-	for i, v := range s {
-		s1[len(s)-i-1] = byte(v)
-	}
-	var s2, _ = strconv.Atoi(string(s1))
-	if isO {
-		s2 = -s2
-	}
-	if s2 > (1<<32/2-1) || s2 < (-1<<32/2) {
+	var ids = 0
+	var m2 = 0
+	var count = len(strconv.Itoa(x)) - 1
+	if count > 10 {
 		return 0
 	}
-	return s2
+	for x > 0 {
+		var m = math.Pow10(count)
+		var y = x / int(m)
+		x -= y * int(m)
+		if m2 == 0 {
+			ids += y
+		} else {
+			ids += y * int(math.Pow10(m2))
+		}
+		m2++
+		count--
+	}
+	if minus {
+		ids = -ids
+	}
+	if ids > (1<<32/2-1) || ids < (-1<<32/2) {
+		return 0
+	}
+	return ids
 }
