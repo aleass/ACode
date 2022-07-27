0e831d7d13efb85e57e64d7faf09a59cc4676817
tuski <g614547908@gmail.com>
2022-07-28 00:37:01 +0800

    demo(0010): committed
    Signed-off-by: tuski <g614547908@gmail.com>

--- leetcode/Go/0010.longest-common-prefix/main.go
+++ leetcode/Go/0010.longest-common-prefix/main.go
@@ -11,58 +11,15 @@ func longestCommonPrefix(strs []string) string {
 	if l < 2 {
 		return strs[0]
 	}
-	c := len(strs[0])
-	var ok = true
-	for j := 0; j < l; /*&& ok*/ j++ {
-		_l := len(strs[j])
-		if _l == 0 {
-			return ""
-		}
-		if strs[j] != strs[0] {
-			ok = false
-		}
-		if _l < c {
-			c = _l
-		}
-	}
-	if ok {
-		return strs[0]
-	}
 
-	i := 0
-	for ; i < c; i++ {
+	for i := 0; i < len(strs[0]); i++ {
 		for j := 1; j < l; j++ {
-			if strs[j][i] != strs[0][i] {
+			if i == len(strs[j]) || strs[j][i] != strs[0][i] {
 				return strs[0][:i]
 			}
 		}
 	}
-	return strs[0][:i]
-}
-
-func longestCommonPrefix2(strs []string) string {
-	var l = len(strs)
-	if l < 2 {
-		return strs[0]
-	}
-	i := 0
-	for c := len(strs[0]); i < c; i++ {
-		var tmp = strs[0][i]
-		for j := 1; j < l; j++ {
-			if len(strs[j]) == 0 {
-				return strs[0][:i]
-			}
-			if strs[j][i] != tmp {
-				return strs[0][:i]
-			}
-			if i == 0 {
-				if _l := len(strs[j]); _l < c {
-					c = _l
-				}
-			}
-		}
-	}
-	return strs[0][:i]
+	return strs[0]
 }
 
 func longestCommonPrefix1(strs []string) string {
